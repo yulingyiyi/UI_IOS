e5610bac0bf5eb063643ea92628903b852067a4f
yulingyiyi <yuling996235005@gmail.com>
2018-05-03 13:54:06 +0800

    RAC demo init

---
+++ RACDemo/RACDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RACDemo
+//
+//  Created by yuling on 2018/5/3.
+//  Copyright © 2018年 yuling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
